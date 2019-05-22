f7f84e348c4df6781a01d3a08fa01ecc979f4b75
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 02:56:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(names)
+  messages = []
+  names.each do |name|
+    messages.push("Hello, my name is #{name}.")
+  end
+  messages
+end
+
+def batch_badge_creator(names)
+
+end
+
+def assign_rooms(names)
+  room_assignment = []
+  room_number = 1
+  names.each do |name|
+    room_assignment.push("Hello, #{name}! You'll be assigned to room #{room_number}!")
+    room_number+=1
+  end
+  room_assignment
+end
